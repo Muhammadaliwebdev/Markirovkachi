704f70d3821212333ee4bcd3a0a67541b6c5c370
Muhammadaliwebdev <goldxack@gmail.com>
2026-05-12 14:39:29 +0500

    multi admin

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 BOT_TOKEN     = os.environ.get("BOT_TOKEN", "")
-ADMIN_ID      = int(os.environ.get("ADMIN_ID", "0"))
+ADMIN_ID = [int(x) for x in os.environ.get("ADMIN_ID", "0").split(",")]
 COMPANIES_FILE  = "firmalar.json"
 TEMPLATES_FOLDER = "shablonlar"
 DOCS_FOLDER    = "markitovkalar"
